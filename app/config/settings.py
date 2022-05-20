c6b83bcf711790b5e599e04bd4d06f85540aeb55
Ubuntu <ubuntu@ip-172-31-4-141.us-east-2.compute.internal>
2022-05-20 04:39:38 +0000

    Configuraciones servidor de producción

--- app/config/settings.py
+++ app/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,9 +26,10 @@ SECRET_KEY = 'django-insecure-scn9n9n)pdii%#0$ji#6mjg!xaquqvhm@4s8=9fj$nzvlr1_^q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
-
+DJANGO_ENV = os.getenv('DJANGO_ENV')
+print(DJANGO_ENV)
 # Application definition
 
 INSTALLED_APPS = [
@@ -79,7 +81,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'locattle-dev',
+        'NAME': 'locattle-dev' if DJANGO_ENV != "prod" else 'locattle',
         'USER': 'admin',
         'PASSWORD': 'encuentrame1',
         'HOST': 'diseno.c7baqt6s9nxp.us-east-2.rds.amazonaws.com',
