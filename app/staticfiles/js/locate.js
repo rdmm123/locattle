d7bac6610deb77386c40f6d2543c32f01fdb315b
Ubuntu <ubuntu@ip-172-31-4-141.us-east-2.compute.internal>
2022-05-20 14:23:58 +0000

    Docker compose correcciones finales

---
+++ app/staticfiles/js/locate.js
@@ -0,0 +1,45 @@
+const initMap = () => {
+    const map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap;',
+        maxZoom: 18,
+    }).addTo(map);
+    return map
+}
+
+const fetchLastLocation = async () => {
+    const response = await fetch(locationEndpoint);
+    const data = await response.json();
+    return data
+}
+
+const formatData = (data) => {
+    const timestampDate = Date(data.timestamp);
+    const formattedDate = timestampDate.toLocaleString('es-CO', {'timeStyle': 'short'});
+    return `
+        <ul>
+            <li><b>Latitud:</b> ${data.lat}</li>
+            <li><b>Longitud:</b> ${data.lng}</li>
+            <li><b>Timestamp:</b> ${timestampDate}</li>
+            <li><b>Vaca:</b> ${data.cow}</li>
+            <li><b>Finca:</b> ${data.farm}</li>
+            <li><b>Departamento:</b> ${data.depto}</li>
+        </ul>
+    `
+}
+
+const setLocation = async (initial=false) => {
+    const data = await fetchLastLocation();
+    const latLng = [data.lat, data.lng];
+    map.setView(latLng, 15);
+    if (initial) {
+        const popup = new L.popup({autoPan: false}).setContent(formatData(data));
+        marker.bindPopup(popup).openPopup();
+    }
+    marker.setLatLng(latLng).addTo(map);
+}
+
+const map = initMap();
+const marker = L.marker([]);
+setLocation(true);
+setInterval(setLocation, 10000);
